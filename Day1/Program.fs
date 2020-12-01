d0b426d99d533ef63ce4ac2befb171ff12ef440f
Nicholas Bannayi <Nick@nicholass-air.home>
2020-12-01 22:46:46 +0000

    Day 1 solution

---
+++ Day1/Program.fs
@@ -0,0 +1,33 @@
+﻿open AoC2020.Common.Utilities
+
+// Advent of Code 2020 day 1.
+
+[<EntryPoint>]
+let main argv =
+
+    // Get puzzle input.
+    let input = "InputFiles/Day1Input.txt" |> Seq.ofFile
+
+    // Get all expenses into an array of int.
+    let expenses = input |> Seq.map (int) |> Array.ofSeq
+
+    // Create list of all expenses pairs.
+    let e1, e2 =
+        [for i in 0..expenses.Length-1 do
+            for j in 0..expenses.Length-1 -> if i <> j then (i,j) else (-1,-1)]
+        |> List.filter (fun (a,b) -> a > 0 && expenses.[a] + expenses.[b] = 2020)
+        |> List.head
+
+    printf "Part 1: result is %d\n" (expenses.[e1] * expenses.[e2]) 
+
+    // Create list of all expenses pairs.
+    let e1, e2, e3 =
+        [for i in 0..expenses.Length-1 do
+            for j in 0..expenses.Length-1 do
+                for k in 0..expenses.Length-1 -> if i <> j && j <> k then (i,j,k) else (-1,-1,-1)]
+        |> List.filter (fun (a,b,c) -> a > 0 && expenses.[a] + expenses.[b] + expenses.[c] = 2020)
+        |> List.head
+
+    printf "Part 2: result is %d\n" (expenses.[e1] * expenses.[e2] * expenses.[e3]) 
+
+    0 
---
+++ Utilities/Seq.fs
@@ -0,0 +1,13 @@
+﻿namespace AoC2020.Common.Utilities
+
+open System.IO
+
+module Seq =
+
+    /// Take lines from an input file and convert to a sequence of strings.
+    let ofFile (filePath : string) = 
+
+        let baseDirectory = Directory.GetParent(__SOURCE_DIRECTORY__)
+        let fullPath = Path.Combine(baseDirectory.FullName, filePath)
+
+        seq { yield! File.ReadLines fullPath }
