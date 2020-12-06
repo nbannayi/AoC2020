509351597ad587d8f5c948447efbff9f0e8b8e63
Nicholas Bannayi <Nick@nicholass-air.home>
2020-12-06 08:15:43 +0000

    Day 6

---
+++ Day6/Program.fs
@@ -0,0 +1,28 @@
+﻿open AoC2020.Common.Utilities
+
+// Advent of Code 2020 day 6.
+    
+[<EntryPoint>]
+let main argv =
+
+    let forms = 
+        "InputFiles/Day6Input.txt"
+        |> Seq.ofFileChunks "\n\n"
+                
+    let totalNoDictinctAnswers =
+        forms
+        |> Seq.map (fun f -> f.Replace("\n",""))
+        |> Seq.map (set)
+        |> Seq.map (Set.count)
+        |> Seq.sum
+        
+    let totalNoSharedAnswers =
+        forms
+        |> Seq.map (fun f -> f.Split('\n') |> Array.map (set))
+        |> Seq.map (Set.intersectMany)
+        |> Seq.map (Set.count)
+        |> Seq.sum
+    
+    printf "Part 1: result is %d\n" totalNoDictinctAnswers    
+    printf "Part 2: result is %d\n" totalNoSharedAnswers    
+    0
