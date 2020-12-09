9b4a9af5f8b7b8a6a245790b05a6419ff41578a2
Nicholas Bannayi <Nick@nicholass-air.home>
2020-12-09 20:56:34 +0000

    Project tidy up

---
+++ Day10/Program.fs
@@ -0,0 +1,14 @@
+﻿open AoC2020.Common.Utilities
+
+// Advent of Code 2020 day 10.
+
+[<EntryPoint>]
+let main argv =
+
+    let input =
+        "InputFiles/Day10Input.txt" 
+        |> Seq.ofFileLines
+
+    printf "%A\n" (input |> Seq.head)
+    printf "%A\n" (input |> Seq.last)
+    0
